a07583a68a3cd93c7e04cc113601b99208d58c88
lphamilton87 <lphamilton87@gmail.com>
2023-04-19 14:46:07 +0100

    added fixtures with testing

--- db/app.js
+++ db/app.js
@@ -1,36 +1,44 @@
-const express = require('express');
+const express = require("express");
 
-const {getTeams, getPlayers, getStats, getLeagues, postNewTeam} = require('./controller')
+const {
+  getTeams,
+  getPlayers,
+  getStats,
+  getLeagues,
+  getFixtures,
+  postNewTeam,
+} = require("./controller");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/teams', getTeams);
-app.get('/api/players', getPlayers);
-app.get('/api/stats', getStats);
-app.get('/api/leagues', getLeagues);
-app.post('/api/teams/', postNewTeam);
+app.get("/api/teams", getTeams);
+app.get("/api/players", getPlayers);
+app.get("/api/stats", getStats);
+app.get("/api/leagues", getLeagues);
+app.get("/api/fixtures", getFixtures);
+app.post("/api/teams/", postNewTeam);
 
 app.use((err, request, response, next) => {
-    if (err.status) {
-      response.status(err.status).send({ msg: err.msg });
-    } else {
-      next(err);
-    }
-  });
-  
-  app.use((err, request, response, next) => {
-    if (err.code === "22P02") {
-      response.status(400).send({ msg: "Bad request" });
-    } else {
-      next(err);
-    }
-  });
-  
-  app.use((err, request, response, next) => {
-    console.log(err);
-    response.status(500).send({ msg: "Internal server error" });
-  });
+  if (err.status) {
+    response.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, request, response, next) => {
+  if (err.code === "22P02") {
+    response.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, request, response, next) => {
+  console.log(err);
+  response.status(500).send({ msg: "Internal server error" });
+});
 
 module.exports = app;
--- db/data/dev_data/index.js
+++ db/data/dev_data/index.js
@@ -2,9 +2,11 @@ const teamData = require('./teams.js')
 const playerData = require('./players.js');
 const leaguesData = require('./leagues.js');
 const playerStatsData = require('./stats.js');
+const fixturesData = require('./fixtures.js')
 module.exports = {
     teamData,
     playerData,
     leaguesData,
     playerStatsData,
+    fixturesData,
 }
